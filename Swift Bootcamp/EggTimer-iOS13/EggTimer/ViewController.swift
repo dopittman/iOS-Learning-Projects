00d8338b8958383fd85c361b65458332c635627a
David Pittman <53283971+dopittman@users.noreply.github.com>
2022-01-11 18:34:50 -0500

    Create directory for Hash Map DS

--- Swift Bootcamp/EggTimer-iOS13/EggTimer/ViewController.swift
+++ Swift Bootcamp/EggTimer-iOS13/EggTimer/ViewController.swift
@@ -10,6 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    let eggTimes : [String : Int] = ["Soft": 5, "Medium": 7, "Hard": 12]
+    var timer = Timer()
+    
 
-
+    @IBAction func HardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        
+        let hardness : String? = sender.currentTitle
+        var secondsRemaining = 0
+        
+        switch hardness {
+        case "Soft":
+            secondsRemaining = 3
+            print(eggTimes["Soft"]!)
+        case "Medium":
+            secondsRemaining = 410
+            print(eggTimes["Medium"]!)
+        case "Hard":
+            secondsRemaining = 12
+            print(eggTimes["Hard"]!)
+        default:
+            print("Error")
+        }
+            
+        
+            
+            timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+                if secondsRemaining > 0 {
+                    print ("\(secondsRemaining) seconds")
+                    secondsRemaining -= 1
+                } else {
+                    Timer.invalidate()
+                    self.titleLabel.text = "DONE!"
+                }
+            }
+        
+    }
+    
 }
