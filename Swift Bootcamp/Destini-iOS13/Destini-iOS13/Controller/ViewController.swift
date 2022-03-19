ef234093bba25413997e5f9922945fc0e5f4d90a
David Pittman <53283971+dopittman@users.noreply.github.com>
2022-03-19 09:10:34 -0400

    Update with more story choices and improved decision tree logic

--- Swift Bootcamp/Destini-iOS13/Destini-iOS13/Controller/ViewController.swift
+++ Swift Bootcamp/Destini-iOS13/Destini-iOS13/Controller/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  Destini-iOS13
-//
-//  Created by Angela Yu on 08/08/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
 
 import UIKit
 
@@ -14,11 +7,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
+    }
+    
+    func updateUI(){
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
     }
 
+    @IBAction func choiceMade(_ sender: UIButton) {
+        storyBrain.updateStory(userChoice: sender.currentTitle!)
+        updateUI()
+        
 
+    }
+    
 }
 
