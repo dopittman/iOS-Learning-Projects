58c662f38919374263b7ed7dd3ae35b0d4080292
David Pittman <53283971+dopittman@users.noreply.github.com>
2022-03-20 14:28:33 -0400

    Add functionality for slider to change label values

--- Swift Bootcamp/BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
+++ Swift Bootcamp/BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+
+    @IBOutlet weak var heightLabelVal: UILabel!
+    @IBOutlet weak var weightLabelVal: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        let height = String(format: "%.2f", heightSlider.value)
+        heightLabelVal.text = "\(height)m"
+    }
+    
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let weight = String(format: "%.0f", weightSlider.value)
+        weightLabelVal.text = "\(weight)m"
+    }
+    
+    @IBAction func calculateBMI(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        let BMIVal = weight / pow(height, 2)
+        print(BMIVal)
+    }
+    
 }
 
