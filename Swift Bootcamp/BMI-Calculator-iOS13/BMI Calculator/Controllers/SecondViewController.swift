0f1b08191446447069646c403570a3b7eb9afad3
David Pittman <53283971+dopittman@users.noreply.github.com>
2022-03-30 18:51:06 -0400

    Create SecondViewController

---
+++ Swift Bootcamp/BMI-Calculator-iOS13/BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by David Pittman on 3/30/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
