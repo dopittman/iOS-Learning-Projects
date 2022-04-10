ed72855c890a61052e4fd2a3204b279b885e9a57
David Pittman <53283971+dopittman@users.noreply.github.com>
2022-04-10 17:37:43 -0400

    Create PersonalCard application boilerplate

---
+++ Swift Bootcamp/PersonalCard/PersonalCard/PersonalCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalCardApp.swift
+//  PersonalCard
+//
+//  Created by David Pittman on 4/10/22.
+//
+
+import SwiftUI
+
+@main
+struct PersonalCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
