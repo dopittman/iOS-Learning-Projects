a5be37efa98dd95fc99f772bb512341444bfe5a2
David Pittman <53283971+dopittman@users.noreply.github.com>
2022-04-10 16:44:40 -0400

    Introduce a SwiftUI I Am Rich application

---
+++ Swift Bootcamp/I Am Rich SUI/I Am Rich SUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I Am Rich SUI
+//
+//  Created by David Pittman on 4/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift Bootcamp/I Am Rich SUI/I Am Rich SUI/I_Am_Rich_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_Rich_SUIApp.swift
+//  I Am Rich SUI
+//
+//  Created by David Pittman on 4/5/22.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_Rich_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
