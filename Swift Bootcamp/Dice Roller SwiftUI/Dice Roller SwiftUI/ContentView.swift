06c92cc6cb2b75f036c0915f2d118ade5184f69a
David Pittman <53283971+dopittman@users.noreply.github.com>
2022-04-14 08:01:30 -0400

    Complete creation of UI layout

--- Swift Bootcamp/Dice Roller SwiftUI/Dice Roller SwiftUI/ContentView.swift
+++ Swift Bootcamp/Dice Roller SwiftUI/Dice Roller SwiftUI/ContentView.swift
@@ -9,13 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(n: 1)
+                    DiceView(n: 4)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button("Roll") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }.font(.system(size: 40, weight: .heavy))
+                    .foregroundColor(.white)
+                    .padding(.all)
+                    .background(Color.red)
+                
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewDevice(PreviewDevice(rawValue: "iPhone 13"))
+    }
+}
+
+struct DiceView: View {
+    
+    let n: Int
+    
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
     }
 }
