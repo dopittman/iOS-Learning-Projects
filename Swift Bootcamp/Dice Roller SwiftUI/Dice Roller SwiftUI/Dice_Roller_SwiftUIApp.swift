f789837ff7b4cbcb7c99fc40d0cf4f60c6dda1c1
David Pittman <53283971+dopittman@users.noreply.github.com>
2022-04-14 06:57:58 -0400

    Create Dice Roller SwiftUI application

---
+++ Swift Bootcamp/Dice Roller SwiftUI/Dice Roller SwiftUI/Dice_Roller_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_Roller_SwiftUIApp.swift
+//  Dice Roller SwiftUI
+//
+//  Created by David Pittman on 4/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Dice_Roller_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
