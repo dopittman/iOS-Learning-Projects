7db91ac26ce6d8b108197fbf5bab873ca164187c
David Pittman <53283971+dopittman@users.noreply.github.com>
2022-04-16 10:09:52 -0400

    Move View Controllers to a new directory to create an MVC model

---
+++ Swift Bootcamp/Tipsy/Tipsy/Controllers/CalculatorViewController.swift
@@ -0,0 +1,33 @@
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    
+    //  Variables
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPercentBtn: UIButton!
+    @IBOutlet weak var tenPercentBtn: UIButton!
+    @IBOutlet weak var twentyPercentBtn: UIButton!
+    @IBOutlet weak var splitNumberLevel: UILabel!
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
