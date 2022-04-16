94f37d01d7db6ce1bf416cdc8e4bdda14d090bd5
David Pittman <53283971+dopittman@users.noreply.github.com>
2022-04-16 11:04:03 -0400

    Add functionality to stepper function to update splitLevelLabel

--- Swift Bootcamp/Tipsy/Tipsy/Controllers/CalculatorViewController.swift
+++ Swift Bootcamp/Tipsy/Tipsy/Controllers/CalculatorViewController.swift
@@ -11,10 +11,24 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var splitNumberLevel: UILabel!
     
     @IBAction func tipChanged(_ sender: UIButton) {
-        
+        if (sender.currentTitle == "0%"){
+            zeroPercentBtn.isSelected = true
+            tenPercentBtn.isSelected = false
+            twentyPercentBtn.isSelected = false
+        } else if (sender.currentTitle == "10%"){
+            zeroPercentBtn.isSelected = false
+            tenPercentBtn.isSelected = true
+            twentyPercentBtn.isSelected = false
+        } else {
+            zeroPercentBtn.isSelected = false
+            tenPercentBtn.isSelected = false
+            twentyPercentBtn.isSelected = true
+        }
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        print(Int(sender.value))
+        splitNumberLevel.text = String(Int(sender.value))
         
     }
     
@@ -26,6 +40,10 @@ class CalculatorViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        zeroPercentBtn.isSelected = false
+        tenPercentBtn.isSelected = false
+        twentyPercentBtn.isSelected = false
+        
     }
 
 
