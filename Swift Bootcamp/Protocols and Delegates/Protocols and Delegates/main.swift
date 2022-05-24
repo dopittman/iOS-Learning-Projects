9d7d34863385270b10eea3e049c7b3e3109614a9
David Pittman <53283971+dopittman@users.noreply.github.com>
2022-05-24 14:25:06 -0400

    Add a functional example of protocol and delegate usage

--- Swift Bootcamp/Protocols and Delegates/Protocols and Delegates/main.swift
+++ Swift Bootcamp/Protocols and Delegates/Protocols and Delegates/main.swift
@@ -7,5 +7,59 @@
 
 import Foundation
 
-print("Hello, World!")
+protocol AdvancedLifeSupport {
+    func performCPR()
+}
+
+class EmergencyCallHandler {
+    var delegate: AdvancedLifeSupport?
+    
+    func assessSituation(){
+        print("Can you tell me what happened?")
+    }
+    
+    func medicalEmergency(){
+        delegate?.performCPR()
+    }
+}
+
+
+struct Paramedic: AdvancedLifeSupport {
+    init(handler: EmergencyCallHandler){
+        handler.delegate = self
+    }
+    
+    func performCPR() {
+        print("The paramedic performs chest compressions, 100 per minute.")
+    }
+}
+
+class Doctor: AdvancedLifeSupport {
+    
+    init(handler: EmergencyCallHandler){
+        handler.delegate = self
+    }
+    
+    func performCPR() {
+        print("Doctor does chest compressions, 100 per minute.")
+    }
+    
+    func useStethescope(){
+        print("Listening for heart sounds.")
+    }
+}
+
+class Surgeon: Doctor {
+    override func performCPR() {
+        super.performCPR()
+        print("He might need surgery too!")
+    }
+}
+
+let john = EmergencyCallHandler()
+let bill = Surgeon(handler: john)
+
+
+john.assessSituation()
+john.medicalEmergency()
 
